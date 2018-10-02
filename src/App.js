6ba5c898bd9cd83ec8c952e382f0658e14564935
Matt <meinzer.matthew@gmail.com>
2018-10-01 20:33:34 -0500

    Add input for cost

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    cost: "Default"
+  }
+  handlePriceChange(e) {
+    this.setState({cost: e.target.value})
+  }
+
   render() {
     return (
       <div className="App">
       <p>Oak Log</p>
+      <input
+        type="text" 
+        value={this.state.cost}
+        onChange={(e) => this.handlePriceChange(e)}
+      />
       </div>
     );
   }
